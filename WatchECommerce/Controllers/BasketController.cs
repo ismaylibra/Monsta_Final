d4a7613a911f4a27a9fe86ad0fc10e11209934b5
abbasoov <ismayiliib@code.edu.az>
2023-01-30 05:45:47 +0400

    added some parts of basket for cookie

---
+++ WatchECommerce/Controllers/BasketController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using Watch.Core.Entities;
+using Watch.DAL.DAL;
+using WatchECommerce.ViewModels;
+
+namespace WatchECommerce.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly WatchDbContext _dbContext;
+
+        public BasketController(WatchDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var basketitems = Request.Cookies["basket"];
+            return  Ok(JsonConvert.DeserializeObject<List<BasketItemViewModel>>(basketitems));
+        }
+
+        public async Task<IActionResult> AddToBasket(int? productId)
+        {
+            if (productId != null) NotFound();
+
+            var product = await _dbContext.Products.Where(p => p.Id == productId)
+                .Include(p => p.ProductImages)
+                .FirstOrDefaultAsync();
+
+            if (product != null) NotFound();
+
+            
+
+            var basket = Request.Cookies["basket"];
+            var basketItems = new List<BasketItemViewModel>();
+
+            var basketItem = new BasketItemViewModel
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price,
+                ProductImages = product.ProductImages,
+                Count = 1
+            };
+
+
+            if (basket is null)
+            {
+             
+                basketItems.Add(basketItem);
+
+            }
+            else
+            {
+                basketItems = JsonConvert.DeserializeObject<List<BasketItemViewModel>>(basket);
+                var existProduct = basketItems.Where(b => b.Id == product.Id).FirstOrDefault();
+                if (existProduct is null)
+                {
+                    basketItems.Add(basketItem);
+                }
+                else
+                {
+                    existProduct.Count += 1;
+                }
+
+            }
+
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(basketItems));
+
+            return Ok();
+        }
+    }
+}
--- WatchECommerce/wwwroot/assets/js/basket.js
+++ WatchECommerce/wwwroot/assets/js/basket.js
@@ -1,7 +1,13 @@
-const addToCartBtns = document.querySelectorAll(".add_to_cart");
+const addToCartBtns = document.querySelectorAll(".add_to_cartBtn");
 
 
-addToCartBtns.forEach(addToCartBtn => addToCartBtn.addEventListener("click", () => {
-    console.log("working");
-}))
+function addToCart(ev) {
+    
+    const productId = ev.target.getAttribute("data-id");
+
+    fetch(`/basket/AddToBasket?productId=${productId}`).then(response => {
+        console.log(response);
+    })
+}
+addToCartBtns.forEach(addToCartBtn => addToCartBtn.addEventListener("click", addToCart))
 
